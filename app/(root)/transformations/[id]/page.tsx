41990d79228446b2c5c9b101aaae2f78563b8a86
Cole <153543951+McFlanky@users.noreply.github.com>
2024-02-22 09:32:57 -0500

    Initial Commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+//localhost:3000/transformations/[id]
